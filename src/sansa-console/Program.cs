8425c27497f0e9acdd41a53f8bb74b0f5fa430d2
benoit74 <benoit.beraud@oviles.info>
2014-03-29 19:53:19 +0100

    Very simple version to explore '/mnt' folder

--- src/sansa-console/Program.cs
+++ src/sansa-console/Program.cs
@@ -1,12 +1,61 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace sansaconsole
 {
-	class MainClass
+	class Program
 	{
+		static Stack<System.IO.DirectoryInfo> directoriesQueue = new Stack<System.IO.DirectoryInfo>();
+		static Int64 nbFiles = 0;
+		static Int64 nbFilesNameChars = 0;
+		static Int64 nbFilesFullNameChars = 0;
+		static Int64 filesTotalSize= 0;
+		static Int64 nbUnauthorizedAccessException= 0;
+		static Int64 nbException= 0;
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			System.IO.DirectoryInfo mainDir = new System.IO.DirectoryInfo("/mnt");
+			directoriesQueue.Push(mainDir);
+
+			TimerCallback tcb = DisplayStatus;
+
+			Timer t = new Timer(tcb,null,1000,1000);
+
+			while (directoriesQueue.Count > 0) {
+				System.IO.DirectoryInfo curDir = directoriesQueue.Pop ();
+				if (Regex.Match(curDir.FullName, @"dosdevices/[A-Za-z]:/").Success)
+				{
+					continue;
+				}
+				try
+				{
+					foreach (System.IO.DirectoryInfo subDir in curDir.EnumerateDirectories()) {
+						directoriesQueue.Push (subDir);
+					}
+					foreach (System.IO.FileInfo file in curDir.EnumerateFiles()) {
+						nbFiles++;
+						nbFilesNameChars += file.Name.ToCharArray().Length;
+						nbFilesFullNameChars += file.FullName.ToCharArray().Length;
+						filesTotalSize += file.Length;
+					}
+				} catch (UnauthorizedAccessException) {
+					nbUnauthorizedAccessException++;
+				} catch (Exception ex) {
+					nbException++;
+					Console.WriteLine ("Unexpected exception for dir '" + curDir.ToString() + "' : " + ex.ToString());
+				} 
+			}
+
+			Console.WriteLine (nbFiles  + " files found ! |" + nbFilesNameChars  + "|" + nbFilesFullNameChars  + "|" + filesTotalSize + "|" + nbUnauthorizedAccessException + "|" + nbException);
+		}
+
+		static void DisplayStatus(Object stateInfo)
+		{
+			Console.WriteLine (directoriesQueue.Peek().ToString() + "|" + directoriesQueue.Count + "|" + nbFiles  + "|" + nbFilesNameChars  + "|" + nbFilesFullNameChars  + "|" + filesTotalSize);
+
 		}
 	}
 }
